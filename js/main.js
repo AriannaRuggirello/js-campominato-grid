ec05bc441cc420e6a838979d4618c1b686180468
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-09 17:12:05 +0200

    print number in console

--- js/main.js
+++ js/main.js
@@ -11,22 +11,23 @@ document.getElementById("play_button").addEventListener("click",
         let visibleGrid = document.querySelector('div.grid_container').classList;
         visibleGrid.remove("hidden");
         visibleGrid.add("visible");
-        console.log(visibleGrid);
+        
 
         // Ogni cella ha un numero progressivo, da 1 a 100.
         for(let i = 1; i <= 100 ; i++){
             // creare l'elemento quadrato all'interno della griglia
             
             const newSquare = createGridSquare('div','square'); 
-            console.log(newSquare);
             
-            let num = i;
-            console.log(num);
 
+            // numero ogni quadratino
+            let num = i;
+            
             newSquare.addEventListener('click', 
             function(){
                 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
                 newSquare.classList.add('clicked-true');
+                console.log(num);
             })
 
             
