8148bcbecd9a8d8895bdc449303cd63e80bb6fe6
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-09 15:31:12 +0200

    js button function , grid visible

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+document.getElementById("play_button").addEventListener("click", 
+    function()
+    {   
+        let play = document.querySelector('div.grid').classList;
+        play.add("visible");
+        
+    });
